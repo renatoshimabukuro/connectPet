25769151f2a9dd1987f3681c04840d4617202d05
Katie Wood <kwood6319@hotmail.co.uk>
2026-02-14 19:02:15 +0900

    Index method OK

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,7 +1,20 @@
 class LogsController < ApplicationController
+  #All actions need users and pets
+  before_action :set_user
+  before_action :set_pet
 
   def index
-    @logs = Log.all
+    @logs = @pet.logs.order(date: :desc)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_pet
+    @pet = @user.pets.find(params[:pet_id])
   end
 
 end
