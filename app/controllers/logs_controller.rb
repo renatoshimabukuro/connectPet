17f80720c4f557ff9dc44974a15bf103faff8d47
Katie Wood <kwood6319@hotmail.co.uk>
2026-02-17 22:48:33 +0900

    Kinda working

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -7,6 +7,23 @@ class LogsController < ApplicationController
     @logs = @pet.logs.order(date: :desc)
   end
 
+  def new
+    @log = @pet.logs.build
+    # Send the attribute names to the form, I think?
+    (1..5).each do |i|
+      @log.send("attr#{i}=", @pet.send("attr#{i}"))
+    end
+  end
+
+  def create
+    @log = @pet.logs.build(log_params)
+    if @log.save
+      redirect_to user_pet_logs_path(@user, @pet), notice: "Log created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   # new - To do
   #
   #create - To do
@@ -23,6 +40,10 @@ class LogsController < ApplicationController
 
   def set_pet
     @pet = @user.pets.find(params[:pet_id])
+  end
+
+  def log_params
+    params.require(:log).permit(:date, :attr1_value, :attr1_memo, :attr2_value, :attr2_memo, :attr3_value, :attr3_memo, :attr4_value, :attr4_memo, :attr5_value, :attr5_memo)
   end
 
 end
