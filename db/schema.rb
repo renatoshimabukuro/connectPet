f2a32860e30eb59adf35d92ed23dc48705b65d10
james newsom <jamespnews95@gmail.com>
2026-02-21 15:30:42 +0900

    log show inline style

--- db/schema.rb
+++ db/schema.rb
@@ -101,9 +101,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_17_130310) do
     t.bigint "chat_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
     t.index ["chat_id"], name: "index_messages_on_chat_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "pets", force: :cascade do |t|
@@ -155,6 +153,5 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_17_130310) do
   add_foreign_key "friendships", "users", column: "vet_id"
   add_foreign_key "logs", "pets"
   add_foreign_key "messages", "chats"
-  add_foreign_key "messages", "users"
   add_foreign_key "pets", "users"
 end
