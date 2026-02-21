f8fd84a4ee27bb2cd79ba789f10c8f0e9dab9179
Katie Wood <kwood6319@hotmail.co.uk>
2026-02-21 12:18:24 +0900

    We got it to consolelog the values

--- app/javascript/controllers/rating_controller.js
+++ app/javascript/controllers/rating_controller.js
@@ -1,7 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["button"]
+
   connect() {
     console.log("Hi")
+  }
+
+  select(event) {
+    event.preventDefault();
+    console.log(event.currentTarget.innerText)
   }
 }
