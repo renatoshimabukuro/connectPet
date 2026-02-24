21d746cea8edf7af413ad4ac53071656f9ff46d7
james newsom <jamespnews95@gmail.com>
2026-02-24 19:51:36 +0900

    add user_id to messages schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_02_17_130310) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_24_104727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -101,7 +101,9 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_17_130310) do
     t.bigint "chat_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["chat_id"], name: "index_messages_on_chat_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "pets", force: :cascade do |t|
@@ -153,6 +155,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_17_130310) do
   add_foreign_key "friendships", "users", column: "vet_id"
   add_foreign_key "logs", "pets"
   add_foreign_key "messages", "chats"
-  add_foreign_key "pets", "users"
   add_foreign_key "messages", "users"
+  add_foreign_key "pets", "users"
 end
