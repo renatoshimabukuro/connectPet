0c2ab36787fd5f9bda4d7bc76433a02ef3153bad
james newsom <jamespnews95@gmail.com>
2026-02-26 20:19:19 +0900

    make log into csv

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,4 +1,5 @@
 class LogsController < ApplicationController
+  require "csv"
   #All actions need users and pets
   before_action :set_user
   before_action :set_pet
@@ -17,6 +18,43 @@ class LogsController < ApplicationController
       redirect_to user_pet_logs_path(@user, @pet), notice: "Log created!"
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def export
+    @logs = @pet.logs
+    start_date = params[:start_date]
+    end_date   = params[:end_date]
+    # set start and end date params in view
+    # get all logs in this range.
+    @logs.where(date:start_date..end_date)
+
+    # create csv file
+    CSV.open("#{@pet.name}Log#{params[:start_date]}To#{params[:end_date]}", "wb") do |csv|
+      header = ["Date"]
+      (1..5).each do |num|
+        attr_name = @pet.send("attr#{num}")
+        if attr_name.present?
+          header += [attr_name, "#{attr_name} Value", "#{attr_name} Memo"]
+        end
+      end
+      # add complete header
+      csv << header
+
+      # itterate over the logs for each csv row
+      @logs.each do |log|
+        row = [log.date]
+        (1..5).each do |num|
+
+          if @pet.send("attr#{num}").present?
+            val_name = log.send("attr#{num}")
+            val_score = log.send("attr#{num}_value")
+            val_memo = log.send("attr#{num}_memo")
+            row += [val_name, val_score, val_memo]
+          end
+        end
+        csv << row
+      end
     end
   end
 
