8e5cbd81ac4c08ef367823cb81baf6ab5cfb276e
Katie Wood <kwood6319@hotmail.co.uk>
2026-03-01 20:32:45 +0900

    Reload is working, but wrong person is sending the message

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,20 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :contents, presence: true
+
+  def from?(user)
+    self.user_id == user.id
+  end
+
+  after_create_commit -> {
+    broadcast_append_to(
+      "chat_#{chat.id}",
+      target: "chat-messages",
+      partial: "messages/message",
+      locals: {
+        message: self,
+        viewer_id: user_id
+      }
+    )
+  }
 end
