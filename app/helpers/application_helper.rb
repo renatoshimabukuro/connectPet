9a5745668af6614e695329340dbcd6f594a10f01
Katie Wood <kwood6319@hotmail.co.uk>
2026-03-08 21:41:53 +0900

    styling and if statements for multi_sortables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,13 +13,36 @@ module ApplicationHelper
       dirs << "asc"
     end
 
-    link_to label,
-      user_pet_logs_path(
-        params[:user_id],
-        params[:pet_id],
-        sort: sorts,
-        dir: dirs
-      )
+    # link_to label,
+    #   user_pet_logs_path(
+    #     params[:user_id],
+    #     params[:pet_id],
+    #     sort: sorts,
+    #     dir: dirs
+    #   )
 
-  end
+    # determine arrow + class
+    current_index = Array(params[:sort]).index(column)
+    arrow = ""
+    css_class = "sort-none"
+
+    if current_index
+      direction = params[:dir][current_index]
+
+      if direction == "asc"
+        arrow = " ↑"
+        css_class = "sort-asc"
+      else
+        arrow = " ↓"
+        css_class = "sort-desc"
+      end
+    end
+
+    link_to "#{label}#{arrow}".html_safe,
+      params.permit(:user_id, :pet_id, sort: [], dir: [])
+            .merge(sort: sorts, dir: dirs),
+      class: css_class
+      end
+
+
 end
