8a7d06ea7ec3a03d7820711a81b7e681afa1d623
Renato Shimabukuro <renato.shimabukuro.2@gmail.com>
2026-03-10 00:28:54 +0900

    clinics index, clinic page style

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,7 +12,7 @@ class PetsController < ApplicationController
       @pets = @user.pets
       @title = "Your Pets!"
     end
-
+    # show connect veterinarians in index page
      @my_clinics = current_user.clinics.where(friendships: {accepted: true}).distinct
   end
 
