5a57b204ffad3ea65c1d9b75e51a420b23a46ccb
Katie Wood <kwood6319@hotmail.co.uk>
2026-03-12 21:06:55 +0900

    Seed hotfix

--- db/seeds.rb
+++ db/seeds.rb
@@ -252,7 +252,7 @@ Log.create!(pet: raye,
 
 Log.create!(pet: raye,
   attr1: "Food", attr1_value: "4", attr1_memo: "45g",
-  attr2: "Nausea", attr2_value: "5", attr2_memo: "",
+  attr2: "Nausea", attr2_value: "2", attr2_memo: "",
   attr3: "Toilet", attr3_value: "5", attr3_memo: "",
   date: Date.parse("2026-02-27"))
 
@@ -264,7 +264,7 @@ Log.create!(pet: raye,
 
 Log.create!(pet: raye,
   attr1: "Food", attr1_value: "5", attr1_memo: "50g",
-  attr2: "Nausea", attr2_value: "5", attr2_memo: "",
+  attr2: "Nausea", attr2_value: "3", attr2_memo: "",
   attr3: "Toilet", attr3_value: "4", attr3_memo: "Small poop",
   date: Date.parse("2026-03-01"))
 
@@ -276,7 +276,7 @@ Log.create!(pet: raye,
 
 Log.create!(pet: raye,
   attr1: "Food", attr1_value: "4", attr1_memo: "40g",
-  attr2: "Nausea", attr2_value: "5", attr2_memo: "",
+  attr2: "Nausea", attr2_value: "3", attr2_memo: "",
   attr3: "Toilet", attr3_value: "5", attr3_memo: "",
   date: Date.parse("2026-03-03"))
 
@@ -288,13 +288,13 @@ Log.create!(pet: raye,
 
 Log.create!(pet: raye,
   attr1: "Food", attr1_value: "5", attr1_memo: "50g",
-  attr2: "Nausea", attr2_value: "5", attr2_memo: "",
+  attr2: "Nausea", attr2_value: "2", attr2_memo: "",
   attr3: "Toilet", attr3_value: "5", attr3_memo: "",
   date: Date.parse("2026-03-05"))
 
 Log.create!(pet: raye,
   attr1: "Food", attr1_value: "5", attr1_memo: "55g",
-  attr2: "Nausea", attr2_value: "5", attr2_memo: "",
+  attr2: "Nausea", attr2_value: "3", attr2_memo: "",
   attr3: "Toilet", attr3_value: "5", attr3_memo: "Healthy poop",
   date: Date.parse("2026-03-06"))
 
