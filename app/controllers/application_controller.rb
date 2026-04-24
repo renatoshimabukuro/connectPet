0d59179c19c002d3777ee3ea6240741c19d5daa4
Katie Wood <kwood6319@hotmail.co.uk>
2026-04-24 17:26:37 +0900

    Updated application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_up_path_for(resource)
+    if resource.vet?
+      new_clinic_path
+    elsif resource.owner?
+      new_pet_path
+    else
+      root_path
+    end
+  end
+
   def after_sign_in_path_for(resource)
-    # sends the user to /users/:id/pets after login
-    user_pets_path(resource)
+    if resource.vet? && resource.clinic.nil?
+      new_clinic_path
+    elsif resource.owner? && resource.pets.empty?
+      new_pet_path
+    else
+      user_pets_path(resource) # or chats_path
+    end
   end
 end
