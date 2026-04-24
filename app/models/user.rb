df1624e774d709ebe97013c0739c11b224a9c830
Katie Wood <kwood6319@hotmail.co.uk>
2026-04-24 17:25:17 +0900

    Updated roles enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   #validations
-  validates :role, presence: true, inclusion:{in:["vet", "owner"], allow_nil:false}
   validates :name, presence: true
   # has many owned chats where user is "owner"
   has_many :owned_chats, class_name: "Chat", foreign_key: :owner_id, dependent: :destroy
@@ -13,6 +12,9 @@ class User < ApplicationRecord
   # has many messages regardless of role and messages are dependent on chat to persist
   has_many :messages, dependent: :destroy
   has_many :pets, dependent: :destroy
+
+  enum role: { owner: "owner", vet: "vet"}
+  validates :role, presence: true
 
   # vet has one clinic
   has_one :clinic, dependent: :destroy
