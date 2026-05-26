e4fbbc3423f52c4b343d2eacbac21ddfc91ae646
Katie Wood <kwood6319@hotmail.co.uk>
2026-05-26 17:18:01 +0900

    Working on fixing style for breeds depending on how many there are

--- app/javascript/controllers/form_steps_controller.js
+++ app/javascript/controllers/form_steps_controller.js
@@ -1,7 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["step", "progressBar", "breedGrid"]
+  static targets = ["step", "progressBar", "breedGrid", "breedPrevButton",
+  "breedNextButton"]
   static values = { current: Number }
 
   connect() {
@@ -38,35 +39,86 @@ export default class extends Controller {
   }
 
   filterBreeds(event) {
-  const speciesId = event.target.value
-  const speciesIcon = event.target.closest("label")
-    ?.querySelector(".icon i")?.className || ""
+    const speciesId = event.target.value
 
-  fetch(`/species/${speciesId}/breeds`)
-    .then(res => res.json())
-    .then(breeds => {
-      const grid = this.breedGridTarget
-      grid.innerHTML = ""
+    const speciesIcon =
+      event.target.closest("label")
+        ?.querySelector(".icon i")
+        ?.className || ""
 
-      if (!breeds.length) {
-        grid.innerHTML = "<p>No breeds found</p>"
-        return
-      }
+    fetch(`/species/${speciesId}/breeds`)
+      .then(res => res.json())
+      .then(breeds => {
+        this.breeds = breeds
+        this.speciesIcon = speciesIcon
+        this.currentBreedPage = 0
+        this.breedsPerPage = 8
 
-      breeds.forEach(breed => {
-        const card = document.createElement("label")
-        card.classList.add("species-card")
-
-        card.innerHTML = `
-          <input type="radio" name="pet[breed_id]" value="${breed.id}" class="hidden" />
-          <div class="card-content">
-            <div class="icon"><i class="${speciesIcon}"></i></div>
-            <div class="name">${breed.name}</div>
-          </div>
-        `
-
-        grid.appendChild(card)
+        this.renderBreedPage()
       })
+  }
+
+  renderBreedPage() {
+    const grid = this.breedGridTarget
+    grid.innerHTML = ""
+
+    if (!this.breeds.length) {
+      grid.innerHTML = "<p>No breeds found</p>"
+      return
+    }
+
+    const start = this.currentBreedPage * this.breedsPerPage
+    const end = start + this.breedsPerPage
+
+    const visibleBreeds = this.breeds.slice(start, end)
+
+    visibleBreeds.forEach(breed => {
+      const card = document.createElement("label")
+      card.classList.add("species-card")
+
+      card.innerHTML = `
+        <input
+          type="radio"
+          name="pet[breed_id]"
+          value="${breed.id}"
+          class="d-none"
+        />
+
+        <div class="card-content">
+          <div class="icon">
+            <i class="${this.speciesIcon}"></i>
+          </div>
+
+          <div class="name">
+            ${breed.name}
+          </div>
+        </div>
+      `
+
+      grid.appendChild(card)
     })
-}
+
+    const lastPage =
+      Math.ceil(this.breeds.length / this.breedsPerPage) - 1
+
+    this.breedPrevButtonTarget.classList.toggle(
+  "d-none",
+  this.currentBreedPage === 0
+)
+
+this.breedNextButtonTarget.classList.toggle(
+  "d-none",
+  this.currentBreedPage >= lastPage
+)
+  }
+
+  nextBreedPage() {
+    this.currentBreedPage++
+    this.renderBreedPage()
+  }
+
+  prevBreedPage() {
+    this.currentBreedPage--
+    this.renderBreedPage()
+  }
 }
