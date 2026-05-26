f6c5d35b7e7121782a07d4a33c9353468cae6ff3
Troy Zangara <troyzangara@gmail.com>
2026-05-26 21:39:26 +0900

    Revert migration to original

--- db/migrate/20260519110355_change_address_in_clinics.rb
+++ db/migrate/20260519110355_change_address_in_clinics.rb
@@ -1,10 +1,5 @@
 class ChangeAddressInClinics < ActiveRecord::Migration[7.1]
   def change
-    unless column_exists?(:clinics, :address)
-      add_column :clinics, :address, :string, default: "", null: false
-    else
-      change_column_null :clinics, :address, false, ""
-      change_column_default :clinics, :address, ""
-    end
+    add_column :clinics, :address, :string, default: "", null: false
   end
 end
