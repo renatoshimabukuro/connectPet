fc83c9a9769313da29069e403df68114a4e17449
Katie Wood <kwood6319@hotmail.co.uk>
2026-05-27 18:16:18 +0900

    Added fix for Heroku migration

--- db/migrate/20260519110355_change_address_in_clinics.rb
+++ db/migrate/20260519110355_change_address_in_clinics.rb
@@ -1,5 +1,6 @@
 class ChangeAddressInClinics < ActiveRecord::Migration[7.1]
   def change
+    remove_column :clinics, :address, :string
     add_column :clinics, :address, :string, default: "", null: false
   end
 end
